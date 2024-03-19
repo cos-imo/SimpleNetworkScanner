81a51b646b2f60ce3ed936a40b331f6cd17b5c45
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-03-19 23:44:41 +0100

    [feat] Prise en charge des options -t et -u (resp. scan TCP et scan UDP)

--- src/parse.c
+++ src/parse.c
@@ -2,7 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "scan.h"
 
+
+int
+parse(int argc, char **argv)
+{
+  int scan_type = 0;
+  int c;
+
+  opterr = 0;
+
+  while ((c = getopt (argc, argv, "tu")) != -1)
+    switch (c)
+      {
+      case 't':
+        scan_type = 0;
+        break;
+      case 'u':
+        scan_type = 1;
+        break;
+      case '?':
+        if (optopt == 'c')
+          fprintf (stderr, "option -%c requires an argument.\n", optopt);
+        else if (isprint (optopt))
+          fprintf(stderr, "unknown option `-%c'.\n", optopt);
+        else
+          fprintf(stderr, "unknown option character `\\x%x'.\n", optopt);
+        return 1;
+      default:
+        abort ();
+      }
+
+  scan(scan_type, 0, 1); 
+
+  return 0;
+}
+
+/*
 int
 main (int argc, char **argv)
 {
@@ -47,3 +84,4 @@ main (int argc, char **argv)
     printf ("non-option argument %s\n", argv[index]);
   return 0;
 }
+*/
