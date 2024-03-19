690bd0983886fa39e060939bfa1a0b61a69f0276
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2024-03-19 23:43:56 +0100

    [feat] Ajout des options de parsing udp et tcp

--- include/parse.h
+++ include/parse.h
@@ -2,6 +2,10 @@
 #define __PARSE_H__
 #include <argp.h>
 
+int parse(int argc, char* *argv);
+
+#define INET_ADDRSTRLEN 3000
+
 struct arguments
 {
     char host[INET_ADDRSTRLEN];
@@ -13,6 +17,8 @@ struct argp_option options[] = {
     {"target", 't', "HOST", 0, "Cible du scan" },
     {"output", 'o', "FILE",  0, "Fichier d'output" },
     {"version", 'v',  0, 0, "Affiche la version et quitte"},
+    {"UDP scan", 'u', 0, 0, "Lance un scan UDP"},
+    {"TCP scan", 't', 0, 0, "Lance un scan TCP"},
     { 0 }
 };
 
